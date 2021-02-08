dfaf71ca99ede31dd619f39b0a29b1afbda2e41d
miaoyumeng <miaoyumeng@sinosoft.com.cn>
2021-02-08 17:46:07 +0800

    获取当前服务器资源占用信息demo

---
+++ moudleDemo/nodeInfo/go.mod
@@ -0,0 +1,11 @@
+module nodeInfo
+
+go 1.15
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/shirou/gopsutil v3.21.1+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ moudleDemo/nodeInfo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+	"time"
+)
+
+/*
+https://blog.csdn.net/whatday/article/details/109620192?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control
+*/
+
+func GetCpuPercent() float64 {
+	percent, _ := cpu.Percent(time.Second, false)
+	return percent[0]
+}
+
+func GetMemPercent() float64 {
+	memInfo, _ := mem.VirtualMemory()
+	return memInfo.UsedPercent
+}
+
+func GetDiskPercent() float64 {
+	parts, _ := disk.Partitions(true)
+	diskInfo, _ := disk.Usage(parts[0].Mountpoint)
+	return diskInfo.UsedPercent
+}
+
+func main() {
+	fmt.Println(GetCpuPercent())
+	fmt.Println(GetMemPercent())
+	fmt.Println(GetDiskPercent())
+}
